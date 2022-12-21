596b83c178a3ccddcf27fc65c0e0216e68eb5041
Philipp Heinrich <philipp@PhilippnrichMBP.fritz.box>
2022-12-21 21:56:05 +0100

    initial astro commit

---
+++ src/site.config.ts
@@ -0,0 +1,11 @@
+const siteMeta = {
+	title: "Philipp Heinrich",
+	description: "",
+	lang: "en-GB",
+	ogLocale: "en_GB",
+	githubUrl: "https://github.com/philipp-heinrich",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
