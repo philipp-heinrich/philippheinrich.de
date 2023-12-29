01905cd25180b621cfca751277d6768c99962b6a
Philipp Heinrich <philipp@heinrich.ltd>
2023-12-29 21:45:14 +0000

    adding minor content changes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 
 export const get = () => rss({
   title: 'Philipp Heinrich | Blog',
-  description: 'My journey learning Astro',
+  description: '',
   site: 'https://philippheinrich.de',
   items: import.meta.glob('./**/*.md'),
   customData: `<language>en-us</language>`,
